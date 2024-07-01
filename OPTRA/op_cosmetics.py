44dbb74cc1989b8c2d867813e3bae7fbf388d863
nitnerolf <nitnerolf@msn.com>
2024-07-01 10:12:50 +0200

    Create op_cosmetics.py

---
+++ OPTRA/op_cosmetics.py
@@ -0,0 +1,21 @@
+##############################################
+# Cosmetics for data
+##############################################
+
+def op_gen_bpm:
+    here_do_domething
+
+def op_apply_bpm:
+    here_do_domething
+
+def op_gen_ffm:
+    here_do_domething
+
+def op_apply_ffm:
+    here_do_domething
+
+def op_load_rawdata:
+    here_do_domething
+
+def op_apply_cosmetics:
+    here_do_domething
