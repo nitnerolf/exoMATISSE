dc207b73f9243ee57eabc27c0d1b3ba9d2d1a0e8
nitnerolf <nitnerolf@msn.com>
2024-07-01 15:47:15 +0200

    Update op_corrflux.py
    add voronin code snippet from Mathis

--- OPTRA/op_corrflux.py
+++ OPTRA/op_corrflux.py
@@ -2,6 +2,9 @@
 # Correlated flux computation
 ##############################################
 
+import numpy as np
+import astropy.constants as const
+
 def op_calc_fft:
     here_do_domething
 
@@ -10,3 +13,89 @@ def op_extract_CF:
 
 def op_sortout_peaks:
     here_do_domething
+
+def op_air_index(wl, T=15, P=1013.25, h=0.1, N_CO2=423, bands='all'):
+""" Compute the refractive index as a function of wavelength at a given temperature,
+        pressure, relative humidity and CO2 concentration, using Equation (5) of Voronin & Zheltikov (2017).
+        
+        Reference: Voronin, A. A. and Zheltikov, A. M. The generalized Sellmeier equation for air. 
+        Sci. Rep. 7, 46111; doi: 10.1038/srep46111 (2017).
+        
+        Inputs:
+        - wl: array of wavelengths in microns,
+        - T: temperature in °C,
+        - P: pressure in hPa,
+        - h: relative humidity,
+        - N_CO2: CO2 concentration in dry air in ppm,
+        - bands: list of absorption bands to include according to the numbering in Table 1 of
+            Voronin & Zheltikov (2017). Default: 'all' (includes all 15 bands).
+        Output:
+        - n_air: array of refractive indices at each wl value.
+"""
+    ## Characteristic wavelengths (microns)
+    wl_abs1 = 1e-3 * np.array([15131, 4290.9, 2684.9, 2011.3, 47862, 6719.0, 2775.6, 1835.6,
+               1417.6, 1145.3, 947.73, 85, 127, 87, 128])
+    wl_abs2 = 1e-3 * np.array([14218, 4223.1, 2769.1, 1964.6, 16603, 5729.9, 2598.5, 1904.8, 
+               1364.7, 1123.2, 935.09, 24.546, 29.469, 22.645, 34.924])
+    
+    ## Absorption coefficients
+    A_abs1 = np.array([4.051e-6, 2.897e-5, 8.573e-7, 1.550e-8, 2.945e-5, 3.273e-6, 
+              1.862e-6, 2.544e-7, 1.126e-7, 6.856e-9, 1.985e-9, 1.2029482,
+              0.26507582, 0.93132145, 0.25787285])
+    A_abs2 = np.array([1.010e-6, 2.728e-5, 6.620e-7, 5.532e-9, 6.583e-8, 3.094e-6,
+              2.788e-6, 2.181e-7, 2.336e-7, 9.479e-9, 2.882e-9, 5.796725,
+              7.734925, 7.217322, 4.742131])
+    
+    ## Conversion
+    T += 273.15 # °C -> K
+    P *= 1e2    # hPa -> Pa
+    
+    ## Water vapor
+    T_cr = 647.096 # critical-point temperature of water (K)
+    P_cr = 22.064e6 # critical-point pressure of water (Pa)
+    tau = T_cr / T
+    th = 1 - T / T_cr
+    a1, a2, a3, a4, a5, a6 = -7.85951783, 1.84408259, -11.7866497, 22.6807411, -15.9618719, 1.80122502
+    # Saturated water vapor pressure (Pa)
+    P_sat = P_cr * np.exp(tau*(a1*th + a2*th**1.5 + a3*th**3 + a4*th**3.5 + a5*th**4 + a6*th**7.5))
+    # Water vapor density (m-3)
+    N_H2O = h * P_sat / (const.k_B.value * T)
+
+    ## Dry air pressure (Pa)
+    P_dry = P - h * P_sat
+
+    ## Gas concentrations (m-3) (mixing ratio in ppm of dry air * concentration of dry air)
+    N_air = P_dry / (const.k_B.value * T)
+    N_N2 = (780840 * 1e-6) * N_air
+    N_O2 = (209460 * 1e-6) * N_air
+    N_Ar = (9340 * 1e-6) * N_air
+    N_CO2 = (N_CO2 * 1e-6) * N_air
+    
+    N_gas = np.zeros_like(wl_abs1)
+    N_gas[0:4] = N_CO2
+    N_gas[4:11] = N_H2O
+    N_gas[11] = N_N2
+    N_gas[12] = N_O2
+    N_gas[13] = N_Ar
+    N_gas[14] = N_H2O
+    
+    ## Critical plasma density (m-3)
+    N_cr = const.m_e.value * const.eps0.value * (2 * np.pi * const.c.value / (const.e.value * wl*1e-6))**2
+   
+    ## Selection of absorption bands
+    if bands == 'all':
+        bands = np.arange(15)
+    else:
+        bands = np.array(bands) - 1
+    
+    ## Refractive index
+    n_air = 1
+    for i_band in bands:
+        dn_air1 = (N_gas[i_band]/N_cr) * A_abs1[i_band] * wl_abs1[i_band]**2 / (wl**2 - wl_abs1[i_band]**2)
+        dn_air2 = (N_gas[i_band]/N_cr) * A_abs2[i_band] * wl_abs2[i_band]**2 / (wl**2 - wl_abs2[i_band]**2)
+        n_air += dn_air1 + dn_air2
+    
+    return n_air
+   
+    
+   
