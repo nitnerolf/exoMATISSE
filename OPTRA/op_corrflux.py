395fa7cc77f47082ebc19dcd40a12181176f5990
nitnerolf <nitnerolf@msn.com>
2024-07-01 10:18:30 +0200

    Create op_corrflux.py

---
+++ OPTRA/op_corrflux.py
@@ -0,0 +1,12 @@
+##############################################
+# Correlated flux computation
+##############################################
+
+def op_calc_fft:
+    here_do_domething
+
+def op_extract_CF:
+    here_do_domething
+
+def op_sortout_peaks:
+    here_do_domething
