74df32ba216bb2ec9c759b9c612a3e774a5c14c2
nitnerolf <nitnerolf@msn.com>
2024-07-01 10:20:31 +0200

    Create op_flux.py

---
+++ OPTRA/op_flux.py
@@ -0,0 +1,18 @@
+##############################################
+# Flux handling methods
+##############################################
+
+def op_extract_beams:
+    here_do_domething
+
+def op_compute_kappa:
+    here_do_domething
+
+def op_apply_kappa:
+    here_do_domething
+
+def op_filter_beams:
+    here_do_domething
+
+def op_compute_flux_fac:
+    here_do_domething
