343f8c7f1dafd18c01d4a9e6fa3f18bbed1a4129
Nathan Saucourt <nathan.saucourt@oca.eu>
2025-05-27 15:33:35 -0400

    rearrangement des scramblings MAT

--- OPTRA/op_instruments.py
+++ OPTRA/op_instruments.py
@@ -9,7 +9,7 @@
 # Project: OPTRA
 #
 ################################################################################
-
+import numpy as np
 #################################################
 # Instrument-specific parameters for VINCI
 #################################################
@@ -41,23 +41,27 @@ op_PIONIER   = {'name':        'MATISSE_L',
 #################################################
 # Instrument-specific parameters for MATISSE
 #################################################
+
+
 op_MATISSE_L = {'name':        'MATISSE_L',
+                'band':        'L',
                 'ntel':        4,
                 'recombination': 'multiaxial_allinone',
                 'interfringe': 17.88*2.75*2*0.85, # in D/lambda
                 'peakwd':      0.9,
                 'scrP':        [1,2,4,3],
-                'scrB':        [[2,3],[0,1],[1,2],[1,3],[0,2],[0,3]],
+                'scrB':        [[2,3],[0,1],[1,2],[1,3],[0,2],[0,3]],# See MATISSE document: MATISSE-TP-TN-003
                 'bcd_offset':  [1,2,3,4,5,6],
                 'ron':         15}
 
 op_MATISSE_N = {'name':        'MATISSE_N',
+                'band':        'N',
                 'ntel': 4, 
                 'recombination': 'multiaxial_allinone',
                 'interfringe': 17.88*2.75*2*0.85, # in D/lambda
                 'peakwd':      0.9,
                 'scrP':[3,4,2,1], 
-                'scrB':[[4,3],[2,1],[3,2],[4,2],[3,1],[4,1]],
+                'scrB': [[3,2],[1,0],[2,1],[3,1],[2,0],[3,0]],# See MATISSE document: MATISSE-TP-TN-003
                 'bcd_offset':[1,2,3,4,5,6],
                 'ron':75}
 
