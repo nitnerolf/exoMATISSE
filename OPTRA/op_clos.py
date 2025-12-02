8ae8386dba72196982d7ba801d883a3822f9c006
Valentin Fleury <fleury.vdhp@gmail.com>
2025-12-02 14:21:04 +0100

    added a print

--- OPTRA/op_clos.py
+++ OPTRA/op_clos.py
@@ -19,7 +19,7 @@ from itertools import combinations
 
 def op_extract_clos(cfdata, verbose=True, plot=False):
     print('Extracting Closure phase and bispectrum amplitude')
-    
+    print('coucou c\'est moi Valentin')
     ntel = cfdata['ntel'] if 'ntel' in cfdata else 6  # Default to 4 if not specified
     triangles = [list(tri) for tri in combinations(range(ntel), 3)]
     print('Triangles:', triangles)
